c4ca1fe6f7613798fdedbb15ae0ad0175f36ee27
cad383 <52176510+cad383@users.noreply.github.com>
2020-05-27 15:01:59 -0400

    Added source code

---
+++ Assign_8/jacobi_iteration_kernel.cu
@@ -0,0 +1,15 @@
+#include "jacobi_iteration.h"
+
+/* FIXME: Write the device kernels to solve the Jacobi iterations */
+
+
+__global__ void jacobi_iteration_kernel_naive()
+{
+    return;
+}
+
+__global__ void jacobi_iteration_kernel_optimized()
+{
+    return;
+}
+
